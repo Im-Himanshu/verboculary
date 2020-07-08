f0bdc5e608c50f546c94b396d9f45e7ec2596ebb
ImHimanShoe <goyalhimanshu414@gmail.com>
2020-07-08 23:06:23 +0530

    adding modified view page

--- src/app/home/view/view.component.ts
+++ src/app/home/view/view.component.ts
@@ -10,10 +10,10 @@ export class ViewComponent implements OnInit {
   constructor() {
 
     this.wordlist = [
-      { word: "first", meaning: "first's meaning", isOpen: false },
-      { word: "Second", meaning: "Second's meaning", isOpen: false },
-      { word: "Third", meaning: "Third's meaning", isOpen: false },
-      { word: "Fourth", meaning: "Fourth's meaning", isOpen: false }
+      { word: "First", meaning: "a very very very long text goes here so that we can see how it wraps with the other element", isOpen: false, isToTakeNote: false, isBookMarked: true },
+      { word: "Second", meaning: "Second's meaning", isOpen: false, isToTakeNote: false, isBookMarked: true },
+      { word: "Third", meaning: "Third's meaning", isOpen: false, isToTakeNote: false, isBookMarked: false },
+      { word: "Fourth", meaning: "Fourth's meaning", isOpen: false, isToTakeNote: false, isBookMarked: true }
     ]
   }
 
@@ -21,6 +21,17 @@ export class ViewComponent implements OnInit {
 
   onWordClickToggleit(word: any) {
     word.isOpen = !word.isOpen;
+  }
+
+  takeNoteToggle(event, word: any) {
+    word.isToTakeNote = !word.isToTakeNote;
+    event.stopPropagation();
+  }
+
+  toggleBookMark(event, word: any) {
+    word.isBookMarked = !word.isBookMarked
+    event.stopPropagation();
+
   }
 
 
