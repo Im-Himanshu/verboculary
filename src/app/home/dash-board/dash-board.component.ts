6f60b641af1fc94d57beaac8b3be94dfd2069e34
ImHimanShoe <goyalhimanshu414@gmail.com>
2020-07-16 00:13:36 +0530

    dashboar UI upgraded

--- src/app/home/dash-board/dash-board.component.ts
+++ src/app/home/dash-board/dash-board.component.ts
@@ -17,14 +17,56 @@ export class DashBoardComponent implements OnInit {
   appNametoUINameMapping = new appNameToUINameMapping().appNametoUINamemapping;
   allSelectedSet;
   allSetProgressData;
+  collectiveProgress;
 
   constructor(private renderer: Renderer2, private db: DatabaseService) {
     this.allSelectedSet = this.db.allSetinSelectedCategory;
     this.allSetProgressData = this.db.allSetData.setLevelProgressData;
+    this.processTotalSetData();
 
   }
 
   ngOnInit() { }
+  processTotalSetData() {
+
+    this.collectiveProgress = {
+      "beginner": {
+        "totalLearned": 0,
+        "totalViewed": 0,
+        "totalWords": 0
+      },
+      "transitional": {
+        "totalLearned": 0,
+        "totalViewed": 0,
+        "totalWords": 0
+      },
+      "pro": {
+        "totalLearned": 0,
+        "totalViewed": 0,
+        "totalWords": 0
+      }
+    }
+
+    for (let one of this.repetitions) {
+      this.collectiveProgress['beginner']['totalLearned'] += this.allSetProgressData[this.allSelectedSet[one]]['totalLearned'];
+      this.collectiveProgress['beginner']['totalWords'] += this.allSetProgressData[this.allSelectedSet[one]]['totalWords'];
+      this.collectiveProgress['beginner']['totalViewed'] += this.allSetProgressData[this.allSelectedSet[one]]['totalViewed'];
+    }
+    for (let one of this.repetitions) {
+      this.collectiveProgress['transitional']['totalLearned'] += this.allSetProgressData[this.allSelectedSet[one + 7]]['totalLearned'];
+      this.collectiveProgress['transitional']['totalWords'] += this.allSetProgressData[this.allSelectedSet[one + 7]]['totalWords'];
+      this.collectiveProgress['transitional']['totalViewed'] += this.allSetProgressData[this.allSelectedSet[one]]['totalViewed'];
+    }
+    for (let one of this.repetitions) {
+      this.collectiveProgress['pro']['totalLearned'] += this.allSetProgressData[this.allSelectedSet[one + 14]]['totalLearned'];
+      this.collectiveProgress['pro']['totalWords'] += this.allSetProgressData[this.allSelectedSet[one + 14]]['totalWords'];
+      this.collectiveProgress['pro']['totalViewed'] += this.allSetProgressData[this.allSelectedSet[one]]['totalViewed'];
+    }
+
+
+
+
+  }
 
 
   scrollParentToChild($child) {
